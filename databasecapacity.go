836c05ac22febfc4d5efc7d54477d74d9f481d03
Geoffrey Bauduin <bauduin.geo@gmail.com>
2017-12-13 17:41:13 +0100

    Adding 'uuid' capacity (#15)
    Signed-off-by: Geoffrey Bauduin <geoffrey.bauduin@corp.ovh.com>

--- databasecapacity.go
+++ databasecapacity.go
@@ -5,18 +5,22 @@ type DatabaseCapacity int8
 
 const (
 	DatabaseCapacitySchema = iota ^ 42
+	DatabaseCapacityUUID
 )
 
 var (
 	databaseCapacities = map[DMS]map[DatabaseCapacity]bool{
 		DatabaseMySQL: {
 			DatabaseCapacitySchema: true,
+			DatabaseCapacityUUID:   false,
 		},
 		DatabasePostgreSQL: {
 			DatabaseCapacitySchema: true,
+			DatabaseCapacityUUID:   true,
 		},
 		DatabaseSqlite3: {
 			DatabaseCapacitySchema: false,
+			DatabaseCapacityUUID:   false,
 		},
 	}
 )
