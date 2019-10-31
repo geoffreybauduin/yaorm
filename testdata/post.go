153f4f9be113899a2900a89b378e6a625a2fe3e7
Antoine Egea <antoine.egea.ext@corp.ovh.com>
2019-10-31 11:21:15 +0100

    Implement Distinct() method in ModelFilter class

--- testdata/post.go
+++ testdata/post.go
@@ -40,15 +40,6 @@ func NewPostFilter() *PostFilter {
 	return &PostFilter{}
 }
 
-func (f *PostFilter) AddOption(opt yaormfilter.RequestOption) yaormfilter.Filter {
-	f.AddOption_(opt)
-	return f
-}
-
-func (f *PostFilter) Distinct() {
-	f.AddOption("SelectDistinct")
-}
-
 func (f *PostFilter) ID(v yaormfilter.ValueFilter) *PostFilter {
 	f.FilterID = v
 	return f
